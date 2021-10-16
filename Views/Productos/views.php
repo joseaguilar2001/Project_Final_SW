ce908cbc1ce21b2fa8f29eac2731c4f00b5f03b7
Acurame <acuramee@gmail.com>
2021-10-15 18:57:51 -0600

    Verificar
    solo verificar si les funciona porfa

--- Views/Productos/views.php
+++ Views/Productos/views.php
@@ -1,56 +1,58 @@
 <section class="section">
-    <div class="container">
-        <div class="columns is-4-tablet is-2-descktop is-centered">
-            <div class="column is-4-tablet is-3-desktop">
-                <div class="card card_deg_green">
-                    <div class="card-content">
-                        <div class="content is-size-3">
-                            Productos Existentes
+    <form action="" method="post">
+        <div class="container">
+            <div class="columns is-4-tablet is-2-descktop is-centered">
+                <div class="column is-4-tablet is-3-desktop">
+                    <div class="card card_deg_green">
+                        <div class="card-content">
+                            <div class="content is-size-3">
+                                Productos Existentes
+                            </div>
                         </div>
+                        <footer class="card-footer">
+                            <p class="card-footer-item">
+                                <span>
+                                <button value="Exsitentes" type="submit" class="button is-link">Ver</button>
+                                </span>
+                            </p>
+                        </footer>
                     </div>
-                    <footer class="card-footer">
-                        <p class="card-footer-item">
-                            <span>
-                                 <a href="#">Ver</a>
-                            </span>
-                        </p>
-                    </footer>
                 </div>
-            </div>
-            <div class="column is-4-tablet is-3-desktop">
-                <div class="card card_deg_warinig">
-                    <div class="card-content">
-                        <div class="content is-size-3">
-                            productos por agotarse
+                <div class="column is-4-tablet is-3-desktop">
+                    <div class="card card_deg_warinig">
+                        <div class="card-content">
+                            <div class="content is-size-3">
+                                productos por agotarse
+                            </div>
                         </div>
+                        <footer class="card-footer">
+                            <p class="card-footer-item">
+                                <span>
+                                <button value="agotarse" type="submit" class="button is-link">Ver</button>
+                                </span>
+                            </p>
+                        </footer>
                     </div>
-                    <footer class="card-footer">
-                        <p class="card-footer-item">
-                            <span>
-                                 <a href="#">Ver</a>
-                            </span>
-                        </p>
-                    </footer>
                 </div>
-            </div>
-            <div class="column is-4-tablet is-3-desktop">
-                <div class="card card_deg_red">
-                    <div class="card-content">
-                        <div class="content is-size-3">
-                           productos sin existensia
+                <div class="column is-4-tablet is-3-desktop">
+                    <div class="card card_deg_red">
+                        <div class="card-content">
+                            <div class="content is-size-3">
+                            productos sin existensia
+                            </div>
                         </div>
+                        <footer class="card-footer">
+                            <p class="card-footer-item">
+                                <span>
+                                <button value="sin existensia" type="submit" class="button is-link">Ver</button>
+                                </span>
+                            </p>
+                        </footer>
                     </div>
-                    <footer class="card-footer">
-                        <p class="card-footer-item">
-                            <span>
-                                 <a href="#">Ver</a>
-                            </span>
-                        </p>
-                    </footer>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 </section>
 
 <section>
@@ -80,24 +82,68 @@
   <tbody>
     <?php foreach($producto as $prod) { ?>
     <?php if($prod -> Estado != '4' || $prod -> Estado < 4){ ?>
-      <tr>
-        <td><?php echo $prod->Name; ?></td>
-        <td><?php echo $prod->Price; ?></td>
-        <td><?php echo $prod->Exist; ?></td>
-        <td><?php echo $prod->Medida; ?></td>
-        <td><?php echo $prod->DateOff; ?></td>
-        <td>
-          <div class="field is-grouped">
-            <p class="control">
-              <a class="button is-warning" href="?controller=productos&action=edit&id=<?php echo $prod->IdProd; ?>">
-                Editar
-              </a>
-            </p>
-          </div>
-        </td>
-      </tr>
-      <?php } ?>
-    <?php } ?>  
+        <?php if ($_POST["Exsitentes"] == "Exsitentes"){?>
+            <?php if( $prod -> Limite < $prod -> Exist){ ?>
+            <tr>
+                <td><?php echo $prod->Name; ?></td>
+                <td><?php echo $prod->Price; ?></td>
+                <td><?php echo $prod->Exist; ?></td>
+                <td><?php echo $prod->Medida; ?></td>
+                <td><?php echo $prod->DateOff; ?></td>
+                <td>
+                <div class="field is-grouped">
+                    <p class="control">
+                    <a class="button is-warning" href="?controller=productos&action=edit&id=<?php echo $prod->IdProd; ?>">
+                        Editar
+                    </a>
+                    </p>
+                </div>
+                </td>
+            </tr>
+            <?php } ?>
+        <?php } ?>
+        <?php if ($_POST["agotarse"] == "agotarse"){?>
+            <?php if($prod -> Exist < $prod -> Limite){ ?>
+            <tr>
+                <td><?php echo $prod->Name; ?></td>
+                <td><?php echo $prod->Price; ?></td>
+                <td><?php echo $prod->Exist; ?></td>
+                <td><?php echo $prod->Medida; ?></td>
+                <td><?php echo $prod->DateOff; ?></td>
+                <td>
+                <div class="field is-grouped">
+                    <p class="control">
+                    <a class="button is-warning" href="?controller=productos&action=edit&id=<?php echo $prod->IdProd; ?>">
+                        Editar
+                    </a>
+                    </p>
+                </div>
+                </td>
+            </tr>
+            <?php } ?>
+        <?php } ?>
+        <?php if ($_POST["sin existensia"] == "sin existensia"){?>
+            <?php if($prod -> Exist == 0){ ?>
+            <tr>
+                <td><?php echo $prod->Name; ?></td>
+                <td><?php echo $prod->Price; ?></td>
+                <td><?php echo $prod->Exist; ?></td>
+                <td><?php echo $prod->Medida; ?></td>
+                <td><?php echo $prod->DateOff; ?></td>
+                <td>
+                <div class="field is-grouped">
+                    <p class="control">
+                    <a class="button is-warning" href="?controller=productos&action=edit&id=<?php echo $prod->IdProd; ?>">
+                        Editar
+                    </a>
+                    </p>
+                </div>
+                </td>
+            </tr>
+            <?php } ?>
+        <?php } ?>
+        <?php } ?>  
+    <?php } ?>
   </tbody>
 </table>
 <tbody>
