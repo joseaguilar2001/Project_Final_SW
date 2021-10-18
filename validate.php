3ec34be6ef7c82a95e2c2a25e9c96d5a150873ab
LTDAre <joanrandy34@gmail.com>
2021-10-18 07:49:38 -0600

    Bulma fixes

--- validate.php
+++ validate.php
@@ -42,10 +42,24 @@ if($filas != null){
     include("login.php");
 
   ?>
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Error al autenticar datos</strong> Porfavor verifica que todos los campos esten en orden
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  <section>
+  <div class="notification">
+  <button class="delete"></button>
+    <strong>ERROR</strong> El usuario o contraseña ingresado son incorrectos
 </div>
+  </section>
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
+
+</script>
   <?php
 }
 mysqli_free_result($resultado);
